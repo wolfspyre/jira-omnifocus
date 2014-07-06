9d3ae8a53e2bc57098d5c970c090b36e3cd604de
Wolf Noble <wnoble@rmn.com>
2014-07-06 02:18:45 -0500

    update script with rudimentary debug level implementation

---
+++ Rakefile
@@ -0,0 +1,8 @@
+task :run do
+  p 'running the syncer'
+  sh '. .rvmrc; bin/jiraomnifocus.rb'
+end
+task :default do
+  p 'Supported options: run'
+  p '    Runs the jiraomnifocus script'
+end
--- bin/jiraomnifocus.rb
+++ bin/jiraomnifocus.rb
@@ -5,6 +5,7 @@ require 'net/http'
 require 'json'
 require 'uri'
 require 'pry'
+require 'pry-remote'
 require 'yaml'
 
 # Put config variables in a file outside the script
@@ -35,16 +36,24 @@ DEBUG = script_config['script_debug']#int. 0-5
 
 
 
+def debug_msg(message='',level=9)
+  if level.to_i < DEBUG.to_i
+    #we should print the debug message
+    p "Debug[#{level}]: #{message}"
+  end
+end
 
 
 # This method gets all issues that are assigned to your USERNAME and whos status isn't Closed or Resolved.  It returns a Hash where the key is the Jira Ticket Key and the value is the Jira Ticket Summary.
 def get_issues(label='',mode)
+  debug_msg("get_issues: #{label}",3)
   case mode
   when 'inclusive'
     uri = URI.parse(JIRA_BASE_URL + "/rest/api/2/search?jql=assignee+%3D+currentUser()+AND+status+not+in+(Closed,+Resolved)+AND+labels+in(#{label})")
+    debug_msg(uri,9)
   when 'exclusive'
     uri = URI.parse(JIRA_BASE_URL + "/rest/api/2/search?jql=assignee+%3D+currentUser()+AND+status+not+in+(Closed,+Resolved)+AND+labels+not+in(#{label})")
- #   binding.pry;
+    debug_msg(uri,9)
   else
     raise StandardError "mode has an unsupported value: #{mode}. should be 'inclusive' or 'exclusive'"
   end
@@ -55,13 +64,16 @@ def get_issues(label='',mode)
 
   Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
     request = Net::HTTP::Get.new(uri.request_uri)
+    debug_msg(request,10)
     request.basic_auth("#{USERNAME}", "#{PASSWORD}")
     response = http.request (request)
-
+     debug_msg(response,10)
     # If the response was good, then grab the data
     if response.code =~ /20[0-9]{1}/
         data = JSON.parse(response.body)
+        debug_msg("DATA: #{data}",9)
         data["issues"].each do |item|
+          debug_msg("ITEM: #{item}",10)
           jira_id = item["key"]
           j_id = item['key']
           j_issue[j_id] = Hash.new
@@ -91,10 +103,12 @@ end
 
 # This method adds a new Task to OmniFocus based on the new_task_properties passed in
 def add_task(omnifocus_document, new_task_properties, task_ext_ticket_key)
+  debug_msg("add_task: #{omnifocus_document} #{new_task_properties} #{task_ext_ticket_key}", 3)
   # If there is a passed in OF project name, get the actual project object
   if new_task_properties['project']
     proj_name = new_task_properties["project"]
     proj = omnifocus_document.flattened_tasks[proj_name]
+    debug_msg("PROJ: #{proj}", 10)
   end
 
 
@@ -110,12 +124,13 @@ def add_task(omnifocus_document, new_task_properties, task_ext_ticket_key)
   # If there is, just stop.
   name   = new_task_properties["name"]
   # binding.pry;
-  exists = proj.tasks.get.find { |entry|  entry.name.get  == name}
+  exists = proj.tasks.get.find { |entry|  debug_msg("exists entry: #{entry}",10); entry.name.get  == name}
   #I tried several means of trying to get this to be a match, alas, I couldn't get it to work easily.
   # problem for future me.
   # /task_ext_ticket_key/ }#entry.name.get =~ /task_ext_ticket_key/ }
   #tasks.get.find.match(task_ext_ticket_key)# { |t|} # t.name == /task_ext_ticket_key/ }
   if exists
+    debug_msg('exists is true',6)
     return false
   end
   #  return false if exists
@@ -124,6 +139,7 @@ def add_task(omnifocus_document, new_task_properties, task_ext_ticket_key)
   if new_task_properties['context']
     ctx_name = new_task_properties["context"]
     ctx = omnifocus_document.flattened_contexts[ctx_name]
+    debug_msg("CTX: #{ctx}", 9)
   end
 
   # Do some task property filtering.  I don't know what this is for, but found it in several other scripts that didn't work...
@@ -149,25 +165,26 @@ end
 
 # This method is responsible for getting your assigned Jira Tickets and adding them to OmniFocus as Tasks
 def add_jira_tickets_to_omnifocus ()
+  debug_msg('add_jira_tickets_to_omnifocus',3)
   #iterate through the projects array, add tickets to the proper project based on the results from the label query.
   #
   #This adds a relationship between your OmniFocus 'projects' and the labels you use in jira. be warned.
 
   PROJECTS.each do |project_label|
-    p "Label: #{project_label}"
+    debug_msg("add_jira_tickets_to_omnifocus: Label: #{project_label}",2)
     # Get the open Jira issues assigned to you
     results = get_issues(project_label,'inclusive')
     if results.nil?
-      puts "No results from Jira matching #{project_label}"
+      debug_msg("add_jira_tickets_to_omnifocus: No results from Jira matching #{project_label}",0)
       exit
     end
 
-    # Get the OmniFocus app and main document via AppleScript
-    omnifocus_app = Appscript.app.by_name("OmniFocus")
-    omnifocus_document = omnifocus_app.default_document
-
+  # Get the OmniFocus app and main document via AppleScript
+   # binding.pry;
+    omnifocus_document = Appscript.app.by_name('/Applications/OmniFocus.app').default_document
     # Iterate through resulting issues.
     results.each do |jira_id, hash|
+      debug_msg("add_jira_tickets_to_omnifocus: Jira_ID: #{jira_id} Hash: #{hash}", 8)
       #binding.pry;
       jira_summary = hash['summary']
       jira_status  = hash['status']
@@ -190,17 +207,19 @@ def add_jira_tickets_to_omnifocus ()
   #get the list of tickets which are assigned to the user, but not caught by the existing label search
   projectstring =  PROJECTS * ","
   exclusive_results = get_issues(projectstring,'exclusive')
-  p 'Fetching tickets not tagged with the labels: ' + projectstring
+  debug_msg("add_jira_tickets_to_omnifocus: Fetching tickets not tagged with the labels: #{projectstring}",1)
   if exclusive_results.nil?
-    puts "No results from Jira which aren't already covered by the previous searches"
+    debug_msg("add_jira_tickets_to_omnifocus: No results from Jira which aren't already covered by the previous searches",0)
     exit
   end
   # TODO: I feel like an idiot duplicating this code. we should change it
   # Get the OmniFocus app and main document via AppleScript
   omnifocus_app = Appscript.app.by_name("OmniFocus")
   omnifocus_document = omnifocus_app.default_document
+  debug_msg("add_jira_tickets_to_omnifocus: omnifocus_document: #{omnifocus_document}", 10)
   # Iterate through resulting issues.
   exclusive_results.each do |jira_id, hash|
+    debug_msg("add_jira_tickets_to_omnifocus: exclusive iterator: jira_id: #{jira_id} hash: #{hash}",11)
     #binding.pry;
     jira_summary = hash['summary']
     jira_status  = hash['status']
@@ -221,12 +240,14 @@ def add_jira_tickets_to_omnifocus ()
 end
 
 def mark_resolved_jira_tickets_as_complete_in_omnifocus ()
+  debug_msg("mark_resolved_jira_tickets_as_complete_in_omnifocus:",3)
   # get tasks from the project
   omnifocus_app = Appscript.app.by_name("OmniFocus")
   omnifocus_document = omnifocus_app.default_document
   ctx = omnifocus_document.flattened_tasks
  #binding.pry;
   ctx.get.each do |task|
+    debug_msg("mark_resolved_jira_tickets_as_complete_in_omnifocus: #{task}",4)
    # p task
     if !task.note.get.match(JIRA_BASE_URL)
      # p 'task.note.get.match failed for '#ß task
@@ -250,7 +271,7 @@ def mark_resolved_jira_tickets_as_complete_in_omnifocus ()
       if tstate == false
         # try to parse out jira id
         # check status of the jira
-        # p " curling for #{jira_id} - Completed: #{tstate}"
+        debug_msg("curling for #{jira_id} - Completed: #{tstate}",6)
         uri = URI.parse(JIRA_BASE_URL + '/rest/api/2/issue/' + jira_id)
         # p uri
         Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
@@ -270,13 +291,14 @@ def mark_resolved_jira_tickets_as_complete_in_omnifocus ()
           end
         end
       else
-        p "not updating status for closed ticket #{jira_id}"
+        debug_msg("not updating status for closed ticket #{jira_id}",1)
       end#end task is not completed
     end
   end
 end
 
 def app_is_running(app_name)
+  debug_msg("app_is_running: #{app_name}",3)
   `ps aux` =~ /#{app_name}/ ? true : false
 end
 
